86a8a14320bc343ccb90aa2d5ddee30196eed6cf
SCAleble Preservation Environments <scape@iapetus.statsbiblioteket.dk>
2012-04-19 15:33:36 +0200

    fixed status. Easier way to create years

--- createYear.sh
+++ createYear.sh
@@ -12,8 +12,12 @@ popd  > /dev/null
 
 source $SCRIPT_PATH/scripts/CONF
 
-for I in '2005' '2006' '2007' '2008' '2009' '2010' '2011'; do \
-	completed=`ls $WORKINGDIR/$I-output | grep tgz | wc -l`
-	scheduled=`wc -l $WORKINGDIR/$I/new?? | tail -1`
-	echo $I: $completed \/ $scheduled;
-done
+YEAR=$1
+YEARLIST=$2
+
+mkdir $SCRIPT_PATH/$YEAR 2> /dev/null
+mkdir $SCRIPT_PATH/$YEAR-output 2> /dev/null
+cp $SCRIPT_PATH/scripts/* $SCRIPT_PATH/$YEAR
+cp $SCRIPT_PATH/scripts/processed.sh $SCRIPT_PATH/$YEAR-output
+cp $YEARLIST $SCRIPT_PATH/$YEAR
+
