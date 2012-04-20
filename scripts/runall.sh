f14dfe18a36770ccb6453cd08fd3ac1a6a86bc57
SCAleble Preservation Environments <scape@iapetus.statsbiblioteket.dk>
2012-04-20 11:01:05 +0200

    Fixed a path bug

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -13,8 +13,9 @@ source $SCRIPT_PATH/CONF
 ESCAPED=`echo $SCRIPT_PATH | sed 's_/_\\\/_g' -`
 
 for I in $(ls $SCRIPT_PATH/new??); do \
+	
 	extension=${I##*.}
 	if [ $I == $extension ]; then 
-		echo "echo $SCRIPT_PATH/run.sh $SCRIPT_PATH/$I \>\& $ESCAPED\/$I.log | at now";
+		echo "echo $SCRIPT_PATH/run.sh $SCRIPT_PATH/`basename $I` \>\& $ESCAPED\/`basename $I`.log | at now";
 	fi
 done
