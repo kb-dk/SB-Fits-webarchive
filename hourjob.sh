60ef7d9eeebabf38289701261ad6d106a53a159c
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-06-26 16:42:31 +0200

    fixed eaten changes

--- hourjob.sh
+++ hourjob.sh
@@ -11,29 +11,14 @@ popd  > /dev/null
 
 source $SCRIPT_PATH/scripts/CONF
 
-
-# copy to public webserver
-#(cd $SCRIPT_PATH/2011-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2011/$I ]; then cp $I /home/scape/public/fits-web-2011/; fi; done)
-#(cd $SCRIPT_PATH/2005-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2005/$I ]; then cp $I /home/scape/public/fits-web-2005/; fi; done)
-#(cd $SCRIPT_PATH/2006-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2006/$I ]; then cp $I /home/scape/public/fits-web-2006/; fi; done)
-#(cd $SCRIPT_PATH/2007-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2007/$I ]; then cp $I /home/scape/public/fits-web-2007/; fi; done)
-#(cd $SCRIPT_PATH/2008-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2008/$I ]; then cp $I /home/scape/public/fits-web-2008/; fi; done)
-#(cd $SCRIPT_PATH/2009-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2009/$I ]; then cp $I /home/scape/public/fits-web-2009/; fi; done)
-#(cd $SCRIPT_PATH/2010-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2010/$I ]; then cp $I /home/scape/public/fits-web-2010/; fi; done)
-# generate filelists on webserver
-#(ls $SCRIPT_PATH/2011-output |grep tgz > /home/scape/public/fits-web-2011/filenames)
-#(ls $SCRIPT_PATH/2005-output |grep tgz > /home/scape/public/fits-web-2005/filenames)
-#(ls $SCRIPT_PATH/2006-output |grep tgz > /home/scape/public/fits-web-2006/filenames)
-#(ls $SCRIPT_PATH/2007-output |grep tgz > /home/scape/public/fits-web-2007/filenames)
-#(ls $SCRIPT_PATH/2008-output |grep tgz > /home/scape/public/fits-web-2008/filenames)
-#(ls $SCRIPT_PATH/2009-output |grep tgz > /home/scape/public/fits-web-2009/filenames)
-#(ls $SCRIPT_PATH/2010-output |grep tgz > /home/scape/public/fits-web-2010/filenames)
-# sum up results
-(cd $SCRIPT_PATH/2011-output; ./processed.sh > /home/scape/public/fits-web-2011/README)
-#(cd $SCRIPT_PATH/2005-output; ./processed.sh > /home/scape/public/fits-web-2005/README)
-(cd $SCRIPT_PATH/2006-output; ./processed.sh > /home/scape/public/fits-web-2006/README)
-(cd $SCRIPT_PATH/2007-output; ./processed.sh > /home/scape/public/fits-web-2007/README)
-(cd $SCRIPT_PATH/2008-output; ./processed.sh > /home/scape/public/fits-web-2008/README)
-(cd $SCRIPT_PATH/2009-output; ./processed.sh > /home/scape/public/fits-web-2009/README)
-(cd $SCRIPT_PATH/2010-output; ./processed.sh > /home/scape/public/fits-web-2010/README)
+for J in '2005' '2006' '2007' '2008' '2009' '2010' '2011'; do 
+	echo "$J"
+	echo "copy to public webserver"
+	(cd $SCRIPT_PATH/$J-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-$J/$I ]; then cp $I /home/scape/public/fits-web-$J/; fi; done)
+	echo  "Generate filelists on webserver"
+	(ls $SCRIPT_PATH/$J-output |grep tgz > /home/scape/public/fits-web-$J/filenames)
+	echo "sum up results"
+	(cd $SCRIPT_PATH/$J; ./processed.sh > /home/scape/public/fits-web-$J/README)
+	echo ""
+done
 
