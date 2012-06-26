8e8b724b5a1a442c05d8988eec431bacd2300466
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-06-26 16:21:00 +0200

    hourjob should work now

--- scripts/processed.sh
+++ scripts/processed.sh
@@ -15,28 +15,28 @@ source $SCRIPT_PATH/CONF
 
 
 
-OBJ=$(ls $OUTPUTDIR/*.tgz | \ 
+OBJ=$(ls $OUTPUTDIR/*.tgz | 
 # list report archives
 	while read I; do  
 # read lines in the list
 		tar tvzf $I | wc -l
 #list the files, count the lines
-	done | \
-	sed 's/^/.+/' | \ 
+	done | 
+	sed 's/^/.+/' | \
 # Add .+ to the start of each line
-	bc | \ 
+	bc | 
 # Add the results (. is the last result)
 	tail -1)
  # Take the last line, which is the total sum
 
 
-B=$(ls -l $OUTPUTDIR/*.tgz | \ 
+B=$(ls -l $OUTPUTDIR/*.tgz | 
 #list report archives
-	awk '{print $5}' | \ 
+	awk '{print $5}' | 
 #extract the size from the list
-	sed 's/^/.+/' | \ 
+	sed 's/^/.+/' | 
 #format for addition
-	bc | \ 
+	bc | 
 #add together
 	tail -1); 
 #only use the last line, being the sum
