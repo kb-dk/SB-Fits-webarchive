446c7479a6b83fd46eae1aee2f394ef21f32aa1e
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-06-26 16:15:44 +0200

    hourjob should work now

--- hourjob.sh
+++ hourjob.sh
@@ -13,21 +13,21 @@ source $SCRIPT_PATH/scripts/CONF
 
 
 # copy to public webserver
-(cd $SCRIPT_PATH/2011-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2011/$I ]; then cp $I /home/scape/public/fits-web-2011/; fi; done)
+#(cd $SCRIPT_PATH/2011-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2011/$I ]; then cp $I /home/scape/public/fits-web-2011/; fi; done)
 #(cd $SCRIPT_PATH/2005-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2005/$I ]; then cp $I /home/scape/public/fits-web-2005/; fi; done)
-(cd $SCRIPT_PATH/2006-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2006/$I ]; then cp $I /home/scape/public/fits-web-2006/; fi; done)
-(cd $SCRIPT_PATH/2007-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2007/$I ]; then cp $I /home/scape/public/fits-web-2007/; fi; done)
-(cd $SCRIPT_PATH/2008-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2008/$I ]; then cp $I /home/scape/public/fits-web-2008/; fi; done)
-(cd $SCRIPT_PATH/2009-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2009/$I ]; then cp $I /home/scape/public/fits-web-2009/; fi; done)
-(cd $SCRIPT_PATH/2010-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2010/$I ]; then cp $I /home/scape/public/fits-web-2010/; fi; done)
+#(cd $SCRIPT_PATH/2006-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2006/$I ]; then cp $I /home/scape/public/fits-web-2006/; fi; done)
+#(cd $SCRIPT_PATH/2007-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2007/$I ]; then cp $I /home/scape/public/fits-web-2007/; fi; done)
+#(cd $SCRIPT_PATH/2008-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2008/$I ]; then cp $I /home/scape/public/fits-web-2008/; fi; done)
+#(cd $SCRIPT_PATH/2009-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2009/$I ]; then cp $I /home/scape/public/fits-web-2009/; fi; done)
+#(cd $SCRIPT_PATH/2010-output; ls | grep tgz | while read I; do if [ ! -f /home/scape/public/fits-web-2010/$I ]; then cp $I /home/scape/public/fits-web-2010/; fi; done)
 # generate filelists on webserver
-(ls $SCRIPT_PATH/2011-output |grep tgz > /home/scape/public/fits-web-2011/filenames)
+#(ls $SCRIPT_PATH/2011-output |grep tgz > /home/scape/public/fits-web-2011/filenames)
 #(ls $SCRIPT_PATH/2005-output |grep tgz > /home/scape/public/fits-web-2005/filenames)
-(ls $SCRIPT_PATH/2006-output |grep tgz > /home/scape/public/fits-web-2006/filenames)
-(ls $SCRIPT_PATH/2007-output |grep tgz > /home/scape/public/fits-web-2007/filenames)
-(ls $SCRIPT_PATH/2008-output |grep tgz > /home/scape/public/fits-web-2008/filenames)
-(ls $SCRIPT_PATH/2009-output |grep tgz > /home/scape/public/fits-web-2009/filenames)
-(ls $SCRIPT_PATH/2010-output |grep tgz > /home/scape/public/fits-web-2010/filenames)
+#(ls $SCRIPT_PATH/2006-output |grep tgz > /home/scape/public/fits-web-2006/filenames)
+#(ls $SCRIPT_PATH/2007-output |grep tgz > /home/scape/public/fits-web-2007/filenames)
+#(ls $SCRIPT_PATH/2008-output |grep tgz > /home/scape/public/fits-web-2008/filenames)
+#(ls $SCRIPT_PATH/2009-output |grep tgz > /home/scape/public/fits-web-2009/filenames)
+#(ls $SCRIPT_PATH/2010-output |grep tgz > /home/scape/public/fits-web-2010/filenames)
 # sum up results
 (cd $SCRIPT_PATH/2011-output; ./processed.sh > /home/scape/public/fits-web-2011/README)
 #(cd $SCRIPT_PATH/2005-output; ./processed.sh > /home/scape/public/fits-web-2005/README)
--- scripts/processed.sh
+++ scripts/processed.sh
@@ -15,20 +15,31 @@ source $SCRIPT_PATH/CONF
 
 
 
-OBJ=$(ls $OUTPUTDIR/*.tgz | \ # list report archives
-	while read I; do \ # read lines in the list
-		tar tvzf $I | wc -l; #list the files, count the lines
+OBJ=$(ls $OUTPUTDIR/*.tgz | \ 
+# list report archives
+	while read I; do  
+# read lines in the list
+		tar tvzf $I | wc -l
+#list the files, count the lines
 	done | \
-	sed 's/^/.+/' | \ # Add .+ to the start of each line
-	bc | \ # Add the results (. is the last result)
-	tail -1) # Take the last line, which is the total sum
+	sed 's/^/.+/' | \ 
+# Add .+ to the start of each line
+	bc | \ 
+# Add the results (. is the last result)
+	tail -1)
+ # Take the last line, which is the total sum
 
 
-B=$(ls -l $OUTPUTDIR/*.tgz | \ #list report archives
-	awk '{print $5}' | \ #extract the size from the list
-	sed 's/^/.+/' | \ #format for addition
-	bc | \ #add together
-	tail -1); #only use the last line, being the sum
+B=$(ls -l $OUTPUTDIR/*.tgz | \ 
+#list report archives
+	awk '{print $5}' | \ 
+#extract the size from the list
+	sed 's/^/.+/' | \ 
+#format for addition
+	bc | \ 
+#add together
+	tail -1); 
+#only use the last line, being the sum
 
 # use bc to calc as megabytes
 MB=$(echo $B/1000000 | bc)
